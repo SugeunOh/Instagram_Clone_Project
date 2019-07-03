bb75c7efd86f7daef2a171a77621e59aee1383e5
ldgeao99 <ldgeao99@gmail.com>
2019-07-03 21:50:57 +0900

    1. 아래의 수정사항 from django.db.models import * -> from django.db import models
    CharField
    -> models.CharField
    
    2. website, bio, phone, gender 필드 추가 및 GENDER_CHOICES 상수 추가

--- insta_clone/insta_clone/users/models.py
+++ insta_clone/insta_clone/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,7 +8,20 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+
+    # Constant
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('not-specified', 'Not specified'),
+    )
+
+    # 확장하여 추가해줄 필드
+    website = models.URLField(null=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(max_length=140, null=True)
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
