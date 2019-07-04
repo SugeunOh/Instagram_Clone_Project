d85ac13e5e9c1ea1f04fe8742ab26c4f99996fbd
ldgeao99 <ldgeao99@gmail.com>
2019-07-04 11:12:34 +0900

    ManyToManyField인 followers, following 필드 추가

--- insta_clone/insta_clone/users/models.py
+++ insta_clone/insta_clone/users/models.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
 
+    """ User Model """
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
@@ -22,6 +23,8 @@ class User(AbstractUser):
     bio = models.TextField(null=True)
     phone = models.CharField(max_length=140, null=True)
     gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
